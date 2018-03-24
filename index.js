b9927d7910e8c140b2cc734871bb6d05f65d8754
Huzaifa Tahir <huzaifa_tah@yahoo.com>
2018-03-24 19:45:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('I claimed your subdomain for Proof of Concept.It will also protect you from bad guys who want to exploit this vulnerability.Check your email for more detail.
+                \n from huzaifa !')
 })
 
 app.listen(app.get('port'), function() {
